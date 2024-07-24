f97c61e7b446c13791f4d714dce2b64a4918594b
lucas <lucasbertoncello1@gmail.com>
2024-07-24 01:37:45 -0300

    ui from home, new and edit finished

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  colors: {
+    background: "#f6f5fc",
+
+    primary: {
+      lighter: "#e0e3ff",
+      light: "#6674f4",
+      main: "#5061fc",
+      dark: "#3346f0",
+    },
+
+    gray: {
+      900: "#222",
+      200: "#bcbcbc",
+    }
+  },
+};
