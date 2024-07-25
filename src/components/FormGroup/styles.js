ba0b1e6ab126ecdd0bdb91e83bc2a32c9f9c54f5
lucas <lucasbertoncello1@gmail.com>
2024-07-25 00:00:40 -0300

    created style and error prop on inputs

--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -4,4 +4,10 @@ export const Container = styled.div`
   & + & {
     margin-top: 16px;
   }
+
+  small {
+    color: ${({ theme }) => theme.colors.danger.main};
+    margin-top: 8px;
+    font-size: 12px;
+  }
 `;
